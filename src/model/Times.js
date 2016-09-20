96896e658ba46ff40e6af094cd3482c0a57dd028
Mitchell Burton <m.burton@metocean.co.nz>
2016-09-20 15:58:49 +1200

    Temp fix to get around code-gen issue.
    Swagger code-gen has an issue with deserializing arrays of primitive types.

--- src/model/Times.js
+++ src/model/Times.js
@@ -20,18 +20,18 @@
 (function(root, factory) {
   if (typeof define === 'function' && define.amd) {
     // AMD. Register as an anonymous module.
-    define(['ApiClient', 'model/Time'], factory);
+    define(['ApiClient'], factory);
   } else if (typeof module === 'object' && module.exports) {
     // CommonJS-like environments that support module.exports, like Node.
-    module.exports = factory(require('../ApiClient'), require('./Time'));
+    module.exports = factory(require('../ApiClient'));
   } else {
     // Browser globals (root is window)
     if (!root.WxTiles) {
       root.WxTiles = {};
     }
-    root.WxTiles.Times = factory(root.WxTiles.ApiClient, root.WxTiles.Time);
+    root.WxTiles.Times = factory(root.WxTiles.ApiClient);
   }
-}(this, function(ApiClient, Time) {
+}(this, function(ApiClient) {
   'use strict';
 
 
@@ -67,7 +67,7 @@
   exports.constructFromObject = function(data, obj) {
     if (data) {
       obj = obj || new exports();
-      ApiClient.constructFromObject(data, obj, Time);
+      ApiClient.constructFromObject(data, obj, 'String');
 
     }
     return obj;
