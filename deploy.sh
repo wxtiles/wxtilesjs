e2fec01384f39907a3277f5e5c28e5bb80be7f13
Mitchell Burton <m.burton@metocean.co.nz>
2016-09-23 15:51:50 +1200

    You need to use npm run to run an npm script.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  prepare-deploy
+  npm run prepare-deploy
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
