cc65d679755e1cba013ae80bba5f1718b5c72301
Mitchell Burton <mitchell.burton@gmail.com>
2016-11-30 12:45:44 +1300

    Revert "Lots of logging to figure out a build issue."
    This reverts commit 986af3e8abdc0413855af7c46aba0e677b7fbd8b.

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ SOURCE_BRANCH="master"
 TARGET_BRANCH="gh-pages"
 
 function doCompile {
-  npm run prepare-deploy --loglevel silly
+  npm run prepare-deploy
 }
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
